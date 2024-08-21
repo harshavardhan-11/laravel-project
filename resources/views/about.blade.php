ec2634f54920ab9736e6e80577ca4b854f0ff5c3
Harshavardhan Singam <harshavardahn singam>
2024-08-21 15:11:49 +0530

    built about as route and a view

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <title>Home page</title>
+    <title>About us Page</title>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
@@ -9,6 +9,6 @@
     <title>Document</title>
 </head>
 <body>
-<h1>Home page</h1>
+<h1>About us page</h1>
 </body>
 </html>
