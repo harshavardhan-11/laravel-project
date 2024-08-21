8338967faf54a5871dc28a18e32b815693cf9b9f
Harshavardhan Singam <harshavardahn singam>
2024-08-21 17:28:38 +0530

    Add Jobs Listing and Job Detail Pages with Dynamic Routing
    - Added a new navigation link "Jobs" to the layout component
    -Created a view to display individual job details

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,30 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Arr;
+
+$jobs = [
+    [
+        'id' => 1,
+        'title' => 'Director',
+        'salary' => '$50,000'
+    ],
+    [
+        'id' => 2,
+        'title' => 'Programmer',
+        'salary' => '$10,000'
+    ],
+    [
+        'id' => 3,
+        'title' => 'Designer',
+        'salary' => '$15,000'
+    ],
+    [
+        'id' => 4,
+        'title' => 'Tester',
+        'salary' => '$15,000'
+    ]
+];
 
 Route::get('/', function () {
     return view('index');
@@ -12,5 +36,16 @@ Route::get('/about', function () {
 
 Route::get('/contact', function () {
     return view('contact');
+});
+
+Route::get('/jobs', function () use($jobs) {
+    return view('jobs', ['jobs' => $jobs]);
+
+});
+
+Route::get('/jobs/{id}', function ($id) use ($jobs) {
+    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+
+    return view('job', ['job' => $job]);
 });
 
