5ffbdf78ad0132a1be6dcf4a7f85e6eb1cd034e4
Harshavardhan Singam <harshavardahn singam>
2024-08-26 10:49:40 +0530

    Implement email notification on job posting

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Job;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\JobPosted;
 
 class JobController extends Controller
 {
@@ -32,11 +34,15 @@ class JobController extends Controller
             'salary' => ['required']
         ]);
 
-        Job::create([
+        $job =  Job::create([
             'title' => request('title'),
             'salary' => request('salary'),
             'employer_id' => 1
         ]);
+
+        Mail::to($job->employer->user)->send(
+            new JobPosted($job)
+        );
 
         return redirect('/jobs');
     }
